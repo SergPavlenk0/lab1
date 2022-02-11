14f54695d3a4ee0570c73cc02b3f2e9fa0119bca
Sergey Pavlenko <sergeipavlenko2020@gmail.com>
2022-02-11 21:37:11 +0200

    first

---
+++ lab1/lab1/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public class MyClass<T> where T : new()
+    {
+        static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
---
+++ lab1/lab1/MyListExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class MyListExtension
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            return list.list;
+        }
+    }
+}
